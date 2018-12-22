8239d369339f8024b72f91cb083014764a8a4576
MatthDuc <m.ducorps@gmail.com>
2018-12-22 16:54:20 +0900

    V0.1 - OS Ok

--- scripts/chocopacks.bat
+++ scripts/chocopacks.bat
@@ -1,6 +1,7 @@
 :: Ensure C:\Chocolatey\bin is on the path
-set /p PATH=<C:\Windows\Temp\PATH
+cmd /c set /p PATH=C:\Windows\Temp\PATH <NULL
 
 :: Install all the things; for example:
+cmd /c choco feature enable -n allowGlobalConfirmation
 cmd /c choco install 7zip
 cmd /c choco install notepadplusplus
